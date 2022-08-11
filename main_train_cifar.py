4b9fa74b642417a9c6436951e73d815440a36557
MurphyWu <waitxxxx@126.com>
2022-08-11 16:24:10 +0800

    Add epochs,workers and resum setting

--- main_train_cifar.py
+++ main_train_cifar.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import platform
 import time
 
 import torch
@@ -7,6 +8,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10, CIFAR100
+from tqdm import tqdm
 
 from data.autoaugment import CIFAR10Policy, Cutout
 from models.CIFAR.models.resnet import resnet20, resnet32
@@ -75,8 +77,11 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', default=128, type=int, help='minibatch size')
     parser.add_argument('--learning_rate', default=1e-2, type=float, help='initial learning_rate')
     parser.add_argument('--T', default=100, type=int, help='snn simulation length')
+    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--workers', type=int, default=8, help='max dataloader workers (per RANK in DDP mode)')
     parser.add_argument('--m', default='normal', type=str, help='calibration type',
                         choices=['normal', 'light', 'advanced'])
+    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
     parser.add_argument('--usebn', action='store_true', help='use batch normalization in ann')
 
     args = parser.parse_args()
@@ -88,7 +93,8 @@ if __name__ == '__main__':
     activation_save_name = args.arch + '_' + args.dataset + '_activation.npy'
     use_cifar10 = args.dataset == 'CIFAR10'
 
-    train_loader, test_loader = build_data(cutout=True, use_cifar10=use_cifar10, auto_aug=True)
+    num_workers = args.workers if not platform.system().lower() == 'windows' else 0
+    train_loader, test_loader = build_data(batch_size, cutout=True, workers=num_workers, use_cifar10=use_cifar10, auto_aug=True)
     best_acc = 0
     best_epoch = 0
     use_bn = args.usebn
@@ -106,19 +112,39 @@ if __name__ == '__main__':
     else:
         raise NotImplementedError
 
-    num_epochs = 300
+    num_epochs = args.epochs
+    start_epoch = 0
     ann.to(device)
+
     criterion = nn.CrossEntropyLoss().to(device)
     # build optimizer
     optimizer = torch.optim.SGD(ann.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4) if use_bn else \
-        torch.optim.SGD(ann.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+                torch.optim.SGD(ann.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, eta_min=0, T_max=num_epochs)
 
-    for epoch in range(num_epochs):
+    if args.resume:
+        ckpt = torch.load(model_save_name)
+        if ckpt.get('best_acc'):
+            best_acc = ckpt['best_acc']
+
+        if ckpt.get('epoch'):
+            start_epoch = ckpt['epoch']
+
+        if ckpt.get('model'):
+            ann.load_state_dict(ckpt['model'])
+
+        if ckpt.get('loss') and ckpt.get('optimizer') and ckpt.get('scheduler'):
+            criterion = ckpt['loss']
+            optimizer = ckpt['optimizer']
+            scheduler = ckpt['scheduler']
+   
+
+    for epoch in range(start_epoch, num_epochs):
         running_loss = 0
         start_time = time.time()
         ann.train()
-        for i, (images, labels) in enumerate(train_loader):
+        pbar = tqdm(enumerate(train_loader))
+        for i, (images, labels) in pbar:
             optimizer.zero_grad()
             labels = labels.to(device)
             images = images.to(device)
@@ -128,10 +154,10 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
             if (i + 1) % 100 == 0:
-                print('Epoch [%d/%d], Step [%d/%d], Loss: %.5f'
-                      % (epoch + 1, num_epochs, i + 1, len(train_loader) // batch_size, running_loss))
+                pbar.set_description('Epoch [%d/%d], Step [%d/%d], Loss: %.5f, Time elapsed: %.3fs'
+                        % (epoch + 1, num_epochs, i + 1, len(train_loader) // batch_size, running_loss/100, time.time() - start_time))
                 running_loss = 0
-                print('Time elapsed:', time.time() - start_time)
+
         scheduler.step()
         correct = 0
         total = 0
@@ -157,6 +183,16 @@ if __name__ == '__main__':
         if best_acc < acc:
             best_acc = acc
             best_epoch = epoch + 1
-            torch.save(ann.state_dict(), model_save_name)
-        print('best_acc is: ', best_acc, ' find in epoch: ', best_epoch)
-        print('Iters:', epoch, '\n\n\n')
+
+            if not os.path.exists(os.path.dirname(model_save_name)):
+                os.makedirs(os.path.dirname(model_save_name))
+
+            file, ext = os.path.splitext(model_save_name)
+            model_best_name = file + '_best' + ext
+            torch.save(ann.state_dict(), model_best_name)
+
+        torch.save({'model': ann.state_dict(), 'epoch': epoch,
+                    'best_acc': best_acc, 'loss': criterion, 'optimizer': optimizer, 'scheduler': scheduler},
+                   model_save_name)
+        print(f'Iters: {epoch + 1} best_acc is: {best_acc} find in epoch: {best_epoch}\n')
+
